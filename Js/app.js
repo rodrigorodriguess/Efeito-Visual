7f9c2e422adad74b8f571493ba6da04e2935d7d8
Rodrigo Rodrigues <69087992+rodrigorodriguess@users.noreply.github.com>
2020-08-05 23:20:05 -0300

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -1,8 +1,8 @@
-// Para fazer com que seja possível fazer isso, primeiro pegamos o texto que desejamos fazer aparecer, depois pegamos a posição do texto ou seja aonde eles está com base no top, por último pegamos a altura da janela.
+// Para fazer com que seja possível fazer isso, primeiro pegamos o texto que desejamos fazer aparecer, depois pegamos a posição do texto ou seja aonde ele está com base no top, por último pegamos a altura da janela.
 
 function scrollAppear() {
   const introText = document.querySelector(".intro-text"); // Pegando o texto que desejo modificar.
-  const introPostion = introText.getBoundingClientRect().top; // Pegando a posição do texto com base no top da janela.
+  const introPostion = introText.getBoundingClientRect().top; // Pegando a posição do texto com base no topo da janela.
   const screenPosition = window.innerHeight / 1.3; // Apenas estamos dividindo para que o texto apareça um pouquinho depois, não é obrigátorio dividir.
 
   // Estamos dizendo que se a posição do texto ( introText ) for menor do que a posição da tela ( introPosition ) ele coloque a classe no texto ( introText )
